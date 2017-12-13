bfe594c23d2f1f239ae0efeb5d23257d403aaa4d
sahabi <sahabi@gmail.com>
2017-12-13 14:44:25 -0600

    modification

--- Mining.hs
+++ Mining.hs
@@ -11,18 +11,6 @@ ratio = diff * 4295032833
 max :: CReal
 max = 115792089237316195423570985008687907853269984665640564039457584007913129639935
 
-trialSuccProb :: CReal
-trialSuccProb = 1 / ratio
-
-minerTrialSuccProb :: CReal -> CReal
-minerTrialSuccProb hp = 1 - ((1 - trialSuccProb)**hp)
-
-minerBlockFailProb :: CReal -> CReal
-minerBlockFailProb miners = (1 - (minerTrialSuccProb (miners * antminer)))**(600)
-
-minerBlockSuccProb :: CReal -> CReal -> CReal
-minerBlockSuccProb blocks miners = 1 - (minerBlockFailProb miners)**blocks
-
 hr :: CReal
 hr =  6
 
@@ -34,6 +22,18 @@ reward = 12.5
 
 antminer :: CReal
 antminer = 14000000000000
+
+trialSuccProb :: CReal
+trialSuccProb = 1 / ratio
+
+minerTrialSuccProb :: CReal -> CReal
+minerTrialSuccProb hashrate = 1 - ((1 - trialSuccProb)**hashrate)
+
+minerBlockFailProb :: CReal -> CReal
+minerBlockFailProb miners = (1 - (minerTrialSuccProb (miners * antminer)))**(600)
+
+minerBlockSuccProb :: CReal -> CReal -> CReal
+minerBlockSuccProb blocks miners = 1 - (minerBlockFailProb miners)**blocks
 
 btcMined :: CReal -> CReal -> CReal
 btcMined blocks miners = reward * (minerBlockSuccProb blocks miners)
